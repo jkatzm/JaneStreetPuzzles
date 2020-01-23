22cc267c5c036fb0cf69713206908c0b1b0243d3
Jordan Katz <jordankatz@Jordans-MacBook-Pro-2.local>
2020-01-23 14:06:19 -0600

    Update JSC_January2020_Solution.py

--- Solved/JSC_January2020_Solution.py
+++ Solved/JSC_January2020_Solution.py
@@ -1,16 +1,4 @@
 
-# def can_win(running_total, memo):
-# 	if running_total not in memo:
-# 		if running_total >= 100:
-# 			memo[running_total] = False
-
-# 		else:
-# 			memo[running_total] = not any([can_win(running_total + i, memo) for i in range(1,11)])
-
-# 	return memo[running_total]
-
-
-
 def winnable_state(current, prev, memo):
 	
 	if (current, prev) not in memo:
@@ -27,6 +15,15 @@ def winnable_state(current, prev, memo):
 	
 	return memo[(current, prev)]
 
+
+memo = {}
+winnable_state(0, -1, memo)
+
+print("\nwinning numbers:")
+
+for i in range(1,100):
+	if winnable_state(i, -1, memo):
+		print(i, ':', winnable_state(i, -1, memo))
 
 # Reasoning
 	# Whoever goes first (in this case, Nate) will win if they say "3".
@@ -45,38 +42,5 @@ def winnable_state(current, prev, memo):
 	# By the same inductive reasoning, if we can say 75, will win. Same goes for 63, 51, 39, 27, 15, and lastly 3.
 
 
-
-
-
-
-winning_numbers = []
-
-memo = {}
-
-# for i in range(1,11):
-# 	print(i, ':', winnable_state(i, -1, memo))
-
-# print()
-
-winnable_state(0, -1, memo)
-
-for k,v in memo.items():
-	if v:
-		print(k)
-
-
-# for i in range(1,100):
-	# if all([(i,j) in memo for j in range(1,11)]):
-# 		if all([memo[i,j] for j in range(1,11)]):
-# 			winning_numbers.append(i)
-
-# for i,j in sorted(memo.keys()):
-	
-# 	print(memo[i,j])
-# 	# if memo[i] == True:
-# 		# print(i, memo[i])
-
-
-print(winning_numbers)
 
 
